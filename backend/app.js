16f7a3e6a0fd2938f99910c0a8f6266d1c7170f6
Vishal Gupta <vishalgupta120703@gmail.com>
2026-02-07 15:16:02 +0530

    Add Authentication

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+
+const express = require("express");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT;
+
+const { connectDB } = require("./config/db");
+connectDB();
+
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const AuthRoutes = require("./routes/authRouter");
+app.use("/api/User", AuthRoutes);
+
+app.listen(port, () => {
+    console.log("the Server is running on port");
+});
+
+
