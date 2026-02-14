f9271466afcbd11886d0565f164a62b1bb72c321
Vishal Gupta <vishalgupta120703@gmail.com>
2026-02-14 11:30:26 +0530

    now backend is ready for deployment

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,14 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT||5000;
 
 const { connectDB } = require("./config/db");
 connectDB();
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
@@ -34,36 +34,5 @@ app.listen(port, () => {
     console.log("the Server is running on port");
 });
 
-
-
-
-
-
-// const express = require("express");
-// const cookieParser = require("cookie-parser");
-// require("dotenv").config();
-
-// const app = express();
-// const port = process.env.PORT;
-
-// const { connectDB } = require("./config/db");
-// connectDB();
-
-// app.use(express.json());  
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
-
-// const AuthRoutes = require("./routes/authRouter");
-// const ProfileRouter = require("./routes/profileRouter")
-// const JobsRouter = require("./routes/jobsRouter")
-// const ApplyRouter = require("./routes/applyRouter")
-// app.use("/api/User", AuthRoutes);
-// app.use("/api/User", ProfileRouter);
-// app.use("/api/User",JobsRouter);
-// app.use("/api/User",ApplyRouter);
-
-// app.listen(port, () => {
-//     console.log("the Server is running on port");
-// });
 
 
